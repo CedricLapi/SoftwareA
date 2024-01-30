6a7717ac516a475e0cecd7830ae565d5c651f668
Cedric Lapi <lapicedric@gmail.com>
2024-01-30 16:42:04 +0100

    postman stuff

--- server/server.js
+++ server/server.js
@@ -2,18 +2,37 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// make sure these lines are above any app.get or app.post code blocks
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+
+
 const movies = [
-    { firstName: "Reimu",  lastName: "Hakurei"    },
-    { firstName: "Marisa", lastName: "Kirisame"   },
-    { firstName: "Sanae",  lastName: "Kochiya"    },
-    { firstName: "Sakuya", lastName: "Izayoi"     },
-    { firstName: "Momiji", lastName: "Inubashiri" }
+    { id: 1, title: "The Shawshank Redemption", year: 1994  },
+    { id: 2, title: "The Godfather", year: 1972   },
+    { id: 3, title: "The Godfather: Part II", year: 1974   },
+    { id: 4, title: "The Dark Knight", year: 2008     },
+    
 ];
 
 app.get("/movie/allMovies", (req, res) => {
     
     res.json(movies);
 });
+
+
+app.get("/movie/:id", (req, res) => {
+    console.log(req.params.id);
+    const movie = movies.find(movie => movie.id === parseInt(req.params.id));
+    res.json(movie);
+});
+
+
+app.post("/movie/newMovie", (req, res) => {
+    movies.push(req.body);
+    res.json({ msg: "ok"});
+})
 
 // this needs to be below the other code blocks
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
